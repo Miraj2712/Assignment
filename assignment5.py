def553ad1abe42ca60777f0706832db25227d81c
Ehsanul Haque Miraz <mirajfin@gmail.com>
2024-10-08 01:46:38 +0600

    Add files via upload

---
+++ assignment5.py
@@ -0,0 +1,9 @@
+def count_vowels(string):
+    vowels = 'aeiouAEIOU'
+    count = sum(1 for character in string if character in vowels)
+    return count
+
+
+String = input("Enter a string :")
+vowel_count = count_vowels(String)
+print(f"The number of vowels in the string is:{vowel_count}")
