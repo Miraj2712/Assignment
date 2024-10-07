139c6858a41547ea6802042308fabffebce8e7cd
Ehsanul Haque Miraz <mirajfin@gmail.com>
2024-10-08 01:55:04 +0600

    Add files via upload

---
+++ assignment8.py
@@ -0,0 +1,13 @@
+class Book:
+    
+    def _init_(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def get_description(self):
+        return f"{self.title} by {self.author}, Pages: {self.pages}"
+
+book1 = Book("1984", "George Orwell", 328)
+print(book1.get_description())
+
